56b0f0eaceb5d9881dcff8adcbabd1eaccb39ae7
DESKTOP-J80L466\Solbi-PC <solbi5555@gmail.com>
2018-05-30 18:22:04 +0900

    안쓰는 import 삭제

--- src/control/EventListServlet.java
+++ src/control/EventListServlet.java
@@ -9,9 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import service.EventService;
-import vo.Event;
-
 /**
  * Servlet implementation class EventListServlet
  */
