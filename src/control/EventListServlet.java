37e80c855b0153c3d7e0b75bfb46bc64f30d1e40
solbi <solbi5555@gmail.com>
2018-05-30 13:04:58 +0900

    event Servlet 완료

--- src/control/EventListServlet.java
+++ src/control/EventListServlet.java
@@ -1,17 +1,23 @@
 package control;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import service.EventService;
+import vo.Event;
 
 /**
  * Servlet implementation class EventListServlet
  */
 public class EventListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	private EventService service = new EventService();
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -24,7 +30,15 @@ public class EventListServlet extends HttpServlet {
 	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		try {
+			List<Event> event = service.findEvent();
+			request.setAttribute("event", event);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		RequestDispatcher rd;
+		String forwardURL = "/admin/admin_calendar.jsp";
+		rd = request.getRequestDispatcher(forwardURL);
+		rd.forward(request, response);
 	}
-
 }
